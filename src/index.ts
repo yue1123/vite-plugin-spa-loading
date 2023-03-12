aaee7a597313461f65f4b67416f2d9df8f227d76
yue1123 <2572468699@qq.com>
2023-03-12 20:57:26 +0800

    feat: refactor

--- src/index.ts
+++ src/index.ts
@@ -2,50 +2,48 @@ import { resolve } from 'node:path'
 import { readFileSync } from 'node:fs'
 import type { Plugin } from 'vite'
 
-interface BaseOptions {
+export interface BaseOptions {
+  rootElementId?: string
   devEnable?: boolean
   css?: string
-  loadingText?: string
+  tipText?: string
   debounce?: number
 }
 
 //  text loading
 // this is default
-export interface TextLoading extends BaseOptions {
-  type: 'text'
-  loadingText: string
-}
+export interface Text extends BaseOptions {}
 
 // provide a image for loading
-export interface ImageLoading extends BaseOptions {
-  type: 'img'
+export interface Image extends BaseOptions {
   src: string
 }
 
 // use svg loading animation for loading
-interface _SvgLoading extends BaseOptions {
-  type: 'svg'
-  svgContent?: string
+interface _Svg extends BaseOptions {
+  content?: string
   path?: string
 }
-export type SvgLoading = _SvgLoading & ({ svgContent: string } | { path: string })
+export type LoadingPlaceholderType = 'text' | 'img' | 'svg'
+export type Svg = _Svg & ({ content: string } | { path: string })
 
-export type Options = TextLoading | ImageLoading | SvgLoading
-export function viteAppLoading(options?: TextLoading): Plugin
-export function viteAppLoading(options?: ImageLoading): Plugin
-export function viteAppLoading(options?: SvgLoading): Plugin
-export function viteAppLoading(
-  options: any = {
-    type: 'text',
-    loadingText: 'loading...'
+export type Options = Text | Image | Svg
+export function spaLoading(type: 'text', options?: Text): Plugin
+export function spaLoading(type: 'img', options?: Image): Plugin
+export function spaLoading(type: 'svg', options?: Svg): Plugin
+export function spaLoading(type: LoadingPlaceholderType = 'text', options: any): Plugin {
+  const defaultOptions = {
+    tipText: 'loading...',
+    rootElementId: 'app',
+    debounce: 300,
+    devEnable: true
   }
-): Plugin {
+  options = Object.assign(defaultOptions, options)
   let root = process.cwd()
-  let appNodeRegexp = /<div id="app">([\w\W]*)<\/div>/gim
-  const defaultOptions = { debounce: 300 }
-  const aniMap = {
-    img: (config: ImageLoading) => `<img src="${config.src}" alt="loading img">`,
-    svg: (config: SvgLoading) => config.svgContent,
+  let appNodeRegexp = new RegExp(`<div id="${options.rootElementId}">([\\w\\W]*)<\\/div>`, 'gim')
+  const aniMap: Record<LoadingPlaceholderType, any> = {
+    img: (config: Image) => `<img src="${config.src}" alt="loading img">`,
+    svg: (config: Svg) => config.content,
     text: () => ''
   }
   const renderTemplate = (config: Options) => {
@@ -57,13 +55,12 @@ export function viteAppLoading(
       0
     )}ms forwards; position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; display: flex; justify-content: center; flex-direction: column; align-items: center; } @keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } } @-moz-keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } } @-webkit-keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } } </style>
     ${config.css ? `<style id="user-css">${config.css}</style>` : ''}
-    <div class="loading-container ${config.type}-loading"><div class="loading-ani">${aniMap[config.type](
-      config as any
-    )}</div>${config.loadingText ? `<div class="loading-text">${config.loadingText}</div>` : ''}</div>`
+    <div class="loading-container ${type}-loading"><div class="loading-ani">${aniMap[type](config)}</div>${
+      config.tipText ? `<div class="loading-text">${config.tipText}</div>` : ''
+    }</div>`
   }
   const tenser = (str: string) => str.replace(/\s{2,99}/g, ' ')
   let isProd = false
-  options = Object.assign(defaultOptions, options)
   return {
     name: 'vite-plugin-spa-loading',
     enforce: 'post',
@@ -72,14 +69,12 @@ export function viteAppLoading(
     },
     transformIndexHtml(index) {
       if (!options.devEnable && !isProd) return
-      if (options.type === 'svg' && options.path) {
-        options.svgContent = readFileSync(resolve(root, options.path)).toString()
+      if (type === 'svg' && options.path) {
+        options.content = readFileSync(resolve(root, options.path)).toString()
       }
-      return tenser(
-        index.replace(appNodeRegexp, (_: string, content: string) => {
-          return `<div id="app">\n${content}\n${renderTemplate(options)}\n<\/div>`
-        })
-      )
+      return index.replace(appNodeRegexp, (_: string, content: string) => {
+        return tenser(`<div id="${options.rootElementId}">\n${content}\n${renderTemplate(options)}\n<\/div>`)
+      })
     }
   }
 }
