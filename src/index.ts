9c004a74263df99714d6a30d1d379cfba68183ed
yue1123 <2572468699@qq.com>
2023-03-31 00:43:25 +0800

    feat: 1. Invalid user file path error 2. Support loading error callback 3. uglify js and html

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,48 @@ import { minify } from 'uglify-js'
 import { minify as htmlMinify } from 'html-minifier'
 
 export interface BaseOptions {
+  /**
+   * app mount element ID
+   * @example vue: <div id="app"></div> ==> app
+   * @example react: <div id="root"></div> ==> root
+   */
   rootElementId?: string
+  /**
+   * Development environment whether enable.
+   * @default true
+   */
   devEnable?: boolean
+  /**
+   * If the existing style is not suitable, you can customize the css through this option
+   */
   css?: string
+  /**
+   * Same as `options.css`, but imported css by file path
+   */
   cssPath?: string
+  /**
+   * Loading placeholder content tip text.
+   * @default Loading...
+   */
   tipText?: string
+  /**
+   * Prevent the loading animation from flashing when the network is good.
+   */
   debounce?: number
+  /**
+   * error tip text
+   * @default ERROR: ...
+   */
+  errorTip?: string
+  /**
+   * error handle callback
+   * @param error error list
+   * @example // error retry
+   *  const search = window.location.search
+        const reloadNum = +search.match(/slr=(\d+)/)?.[1] || 1
+        if (reloadNum < 3) window.location.search = `slt=${Date.now()}&slr=${reloadNum + 1}`
+   * @returns void
+   */
   onError?: (error: string[]) => void
 }
 
@@ -20,14 +56,24 @@ export interface Text extends BaseOptions {}
 
 // provide a image for loading
 export interface Image extends BaseOptions {
+  /**
+   * Specify the file path of the image. It is recommended to use `base64` images, avoid network loading affecting rendering.
+   */
   src: string
 }
 
 // use svg loading animation for loading
 interface _Svg extends BaseOptions {
+  /**
+   * Specify the svg content
+   */
   content?: string
+  /**
+   * Specify the svg file path
+   */
   path?: string
 }
+
 export type LoadingPlaceholderType = 'text' | 'img' | 'svg'
 export type Svg = _Svg & ({ content: string } | { path: string })
 
@@ -43,11 +89,12 @@ export function spaLoading(type: LoadingPlaceholderType = 'text', options: any):
     devEnable: true,
     css: '',
     cssPath: '',
+    errorTip: 'ERROR: ',
     onError: () => {}
   }
   options = Object.assign(defaultOptions, options)
-  let root = process.cwd()
 
+  let root = process.cwd()
   let appNodeRegexp = new RegExp(`<div id="${options.rootElementId}">([\\w\\W]*)<\\/div>`, 'gim')
   const aniMap: Record<LoadingPlaceholderType, any> = {
     img: (config: Image) => `<img src="${config.src}" alt="loading img">`,
@@ -85,63 +132,77 @@ export function spaLoading(type: LoadingPlaceholderType = 'text', options: any):
     const container = document.getElementById('vite-plugin-spa-loading');
     if (container) {
       container.innerHTML =
-        '<pre class="vite-plugin-spa-loading-error">ERROR: \\n\\n' + errorList.join('\\n') + '</pre>';
+        '<pre class="vite-plugin-spa-loading-error">${config.errorTip}\\n\\n' + errorList.join('\\n') + '</pre>';
     }
   }
-} catch (err) {}`
-    const html = `<style   id="internal-css">
-      .vite-plugin-spa-loading-error{
-        color: #b75555;
-      } 
-      .loading-container {
-        opacity: 0; 
-        animation: fade-in ${halfDebounce + 100}ms linear ${halfDebounce}ms forwards; 
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100vw; 
-        height: 100vh; 
-        display: flex; 
-        justify-content: center; 
-        flex-direction: column; 
-        align-items: center; 
-      }
-      @keyframes fade-in {
-        0% { 
-          opacity: 0;
+  } catch (err) {}`
+    const html = `
+      <style id="internal-css">
+        .vite-plugin-spa-loading-error{
+          color: #b75555;
         } 
-        100% { 
-          opacity: 1;
-        } 
-      } 
-      @-moz-keyframes fade-in {
-        0% { 
+        .loading-container {
           opacity: 0; 
+          animation: fade-in ${halfDebounce + 100}ms linear ${halfDebounce}ms forwards; 
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100vw; 
+          height: 100vh; 
+          display: flex; 
+          justify-content: center; 
+          flex-direction: column; 
+          align-items: center; 
+        }
+        @keyframes fade-in {
+          0% { 
+            opacity: 0;
+          } 
+          100% { 
+            opacity: 1;
+          } 
         } 
-        100% { 
-          opacity: 1;
+        @-moz-keyframes fade-in {
+          0% { 
+            opacity: 0; 
+          } 
+          100% { 
+            opacity: 1;
+          } 
         } 
-      } 
-      @-webkit-keyframes fade-in {
-        0% { 
-          opacity: 0; 
-        } 
-        100% { 
-          opacity: 1; 
-        } 
-      }
-    </style>
-    ${config.css ? `<style id="user-css">${config.css}</style>` : ''}\n ${
-      externalStyle !== '' ? `<style id="external-css">${externalStyle}</style>` : ''
-    }
-    <div id="vite-plugin-spa-loading" class="loading-container ${type}-loading">
-      <div class="loading-ani">${aniMap[type](config)}</div>
-      ${config.tipText ? `<div class="loading-text">${config.tipText}</div>` : ''}
-    </div>`
+        @-webkit-keyframes fade-in {
+          0% { 
+            opacity: 0; 
+          } 
+          100% { 
+            opacity: 1; 
+          } 
+        }
+      </style>
+      ${config.css ? `<style id="user-css">${config.css}</style>` : ''}
+      ${externalStyle !== '' ? `<style id="external-css">${externalStyle}</style>` : ''}
+      <div id="vite-plugin-spa-loading" class="loading-container ${type}-loading">
+        <div class="loading-ani">${aniMap[type](config)}</div>
+        ${config.tipText ? `<div class="loading-text">${config.tipText}</div>` : ''}
+      </div>`
 
-    return `${true ? htmlMinify(html) : html}
-    <script>${isProd ? minify(script).code : script}</script>
-    `
+    return `
+      ${isProd ? htmlMinify(html, { minifyCSS: true }) : html}
+      <script>${isProd ? minify(script).code : script}</script>`
+  }
+
+  const errorLog = (content: string) => {
+    console.log('\n')
+    console.log('\x1b[31m%s%s\x1b[0m', '✘ [vite-plugin-spa-loading] - ', content)
+    console.log()
+  }
+  const getFileContent = (src: string) => {
+    try {
+      return readFileSync(src, { encoding: 'utf-8' }).toString()
+    } catch (error: any) {
+      errorLog(error.message)
+      return ''
+    }
   }
   return {
     name: 'vite-plugin-spa-loading',
@@ -152,15 +213,16 @@ export function spaLoading(type: LoadingPlaceholderType = 'text', options: any):
     transformIndexHtml(index) {
       if (!options.devEnable && !isProd) return
       if (type === 'svg' && options.path) {
-        options.content = readFileSync(resolve(root, options.path)).toString()
+        options.content = getFileContent(resolve(root, options.path))
       }
-
       let cssContent: string = ''
       if (options.cssPath !== '') {
-        cssContent = readFileSync(resolve(root, options.cssPath)).toString()
+        cssContent = getFileContent(resolve(root, options.cssPath))
       }
       return index.replace(appNodeRegexp, (_: string, content: string) => {
-        return `<div id="${options.rootElementId}">\n${content}\n${renderTemplate(options, cssContent)}\n<\/div>`
+        return `<div id="${options.rootElementId}">
+          ${content}${renderTemplate(options, cssContent)}
+    <\/div>`
       })
     }
   }
